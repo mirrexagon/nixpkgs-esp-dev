3e2eeb9c35b43a641c7f86c66a5df30d7a324346
Andrew Abbott <andrew@mirx.dev>
2023-06-23 17:22:05 +1000

    Update ESP32-S3 toolchain
    I missed this one when I was updating the others.

--- pkgs/esp32s3-toolchain-bin.nix
+++ pkgs/esp32s3-toolchain-bin.nix
@@ -1,6 +1,7 @@
 # This version needs to be compatible with the version of ESP-IDF specified in `esp-idf/default.nix`.
-{ version ? "2021r2-patch3"
-, hash ? "sha256-WbJx0BT/ORW22xtDthCkXuoV/l1od9EsrooZHMmW7Tc="
+{ version ? "2022r1"
+, hash ? "sha256-UFiy5yQWbDTKCewtU3c1AlLei85QObBsADUvmoFR924="
+, gccVersion ? "11_2_0"
 , stdenv
 , lib
 , fetchurl
@@ -23,7 +24,7 @@ stdenv.mkDerivation rec {
   inherit version;
 
   src = fetchurl {
-    url = "https://github.com/espressif/crosstool-NG/releases/download/esp-${version}/xtensa-esp32s3-elf-gcc8_4_0-esp-${version}-linux-amd64.tar.gz";
+    url = "https://github.com/espressif/crosstool-NG/releases/download/esp-${version}/xtensa-esp32s3-elf-gcc${gccVersion}-esp-${version}-linux-amd64.tar.xz";
     inherit hash;
   };
 
