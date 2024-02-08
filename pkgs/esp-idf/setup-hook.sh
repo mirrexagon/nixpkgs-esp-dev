3dc212705b4be73004893cd832e842ea1be27e2b
Sam Willcocks <sam@wlcx.cc>
2024-02-08 23:21:17 +0000

    Set IDF_TOOLS_PATH

--- pkgs/esp-idf/setup-hook.sh
+++ pkgs/esp-idf/setup-hook.sh
@@ -4,9 +4,10 @@ addIdfEnvVars() {
     # Crude way to detect if $1 is the ESP-IDF derivation.
     if [ -e "$1/tools/idf.py" ]; then
         export IDF_PATH="$1"
+        export IDF_TOOLS_PATH="$IDF_PATH/tools"
         export IDF_PYTHON_CHECK_CONSTRAINTS=no
         export IDF_PYTHON_ENV_PATH="$IDF_PATH/python-env"
-        addToSearchPath PATH "$IDF_PATH/tools"
+        addToSearchPath PATH "$IDF_TOOLS_PATH"
     fi
 }
 
