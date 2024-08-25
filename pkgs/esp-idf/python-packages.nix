2356d69405b5c3ff19ebdb869d1174fb829c703b
Mathieu Rene <mathieu.rene@gmail.com>
2024-08-25 16:22:19 -0400

    Update python-packages

--- pkgs/esp-idf/python-packages.nix
+++ pkgs/esp-idf/python-packages.nix
@@ -14,23 +14,18 @@ with pythonPackages;
 rec {
   idf-component-manager = buildPythonPackage rec {
     pname = "idf-component-manager";
-    version = "1.3.2";
+    version = "2.0.2";
+    pyproject = true;
 
-    src = fetchFromGitHub {
-      owner = "espressif";
-      repo = pname;
-      rev = "v${version}";
-      sha256 = "sha256-rHZHlvRKMZvvjf3S+nU2lCDXt0Ll4Ek04rdhtfIQ1R0=";
+    src = fetchPypi {
+      inherit version;
+      pname = "idf_component_manager";
+      sha256 = "sha256-aPpejbLkbm7oExy0QuC6ZqGYBK4OpYWM0zQnnfkhcsU=";
     };
 
-    # For some reason, this 404s.
-    /*
-      src = fetchPypi {
-      inherit pname version;
-      sha256 = "sha256-12ozmQ4Eb5zL4rtNHSFjEynfObUkYlid1PgMDVmRkwY=";
-      };
-    */
-
+    build-system = [
+      setuptools
+    ];
     doCheck = false;
 
     propagatedBuildInputs = [
@@ -38,6 +33,14 @@ rec {
       cffi
       click
       colorama
+      jsonref
+
+      pydantic
+      pydantic-core
+      pydantic-core
+      pydantic-settings
+      pyparsing
+
       packaging
       pyyaml
       requests
@@ -46,14 +49,8 @@ rec {
       schema
       six
       tqdm
+      typing-extensions
     ] ++ cachecontrol.optional-dependencies.filecache;
-
-    # setup.py says it needs these deps, but it actually doesn't. contextlib2
-    # isn't supported on some pythons and urllib3 is pinned to an old version
-    postPatch = ''
-      sed -i '/contextlib2/d' setup.py
-      sed -i '/urllib3/d' setup.py
-    '';
 
     meta = {
       homepage = "https://github.com/espressif/idf-component-manager";
@@ -62,12 +59,17 @@ rec {
 
   esp-coredump = buildPythonPackage rec {
     pname = "esp-coredump";
-    version = "1.5.2";
+    version = "1.11.0";
+    pyproject = true;
 
     src = fetchPypi {
       inherit pname version;
-      sha256 = "sha256-hQkXnGoAXCLk/PV7Y+C0hOgXGRY77zbIp2ZDC0cxfLo=";
+      sha256 = "sha256-xCt9TsTWklD0GcHfI9gHQiVhADZFjmiPQaleE5HdBNc=";
     };
+
+    build-system = [
+      setuptools
+    ];
 
     doCheck = false;
 
@@ -84,11 +86,11 @@ rec {
 
   esptool = buildPythonPackage rec {
     pname = "esptool";
-    version = "4.6.2";
+    version = "4.7.0";
 
     src = fetchPypi {
       inherit pname version;
-      sha256 = "sha256-VJ75Pu9C7n6UYs5aU8Ft96DHHZGz934Z7BV0mATN8wA=";
+      sha256 = "sha256-AUVOaeHvNgEhXbg/8ssfx57OZ9JLDl1D1FG0EER8SJM=";
     };
 
     doCheck = false;
@@ -109,17 +111,28 @@ rec {
 
   esp-idf-kconfig = buildPythonPackage rec {
     pname = "esp-idf-kconfig";
-    version = "1.1.0";
+    version = "2.3.0";
+    pyproject = true;
+
+    build-system = [
+      setuptools
+    ];
 
     src = fetchPypi {
-      inherit pname version;
-      sha256 = "sha256-s8ZXt6cf5w2pZSxQNIs/SODAUvHNgxyQ+onaCa7UbFA=";
+      inherit version;
+      pname = "esp_idf_kconfig";
+      sha256 = "sha256-n+8QM5xe+c8UFl8dTndRTBd4QW7nG1NWiYiEdll06wg=";
     };
 
     doCheck = false;
 
     propagatedBuildInputs = [
       kconfiglib
+
+      # These packages aren't declared as dependencies but will fail idf.py's
+      # intial depenency check.
+      intelhex
+      rich
     ];
 
     meta = {
@@ -129,12 +142,17 @@ rec {
 
   esp-idf-monitor = buildPythonPackage rec {
     pname = "esp-idf-monitor";
-    version = "1.1.1";
+    version = "1.4.0";
+    pyproject = true;
 
     src = fetchPypi {
       inherit pname version;
-      sha256 = "sha256-c62X3ZHRShhbAFmuPc/d2keqE9T9SXYIlJTyn32LPaE=";
+      sha256 = "sha256-CaX/eA16f4LBU6nUwrIrnlRw2s7CGezOlrHD3dpP8ok=";
     };
+    
+    build-system = [
+      setuptools
+    ];
 
     doCheck = false;
 
@@ -142,6 +160,7 @@ rec {
       pyserial
       esp-coredump
       pyelftools
+      esp-idf-panic-decoder
     ];
 
     meta = {
@@ -151,11 +170,11 @@ rec {
 
   esp-idf-size = buildPythonPackage rec {
     pname = "esp-idf-size";
-    version = "0.3.1";
+    version = "1.5.0";
 
     src = fetchPypi {
       inherit pname version;
-      sha256 = "sha256-OzthhzKGjyqDJrmJWs4LMkHz0rAwho+3Pyc2BYFK0EU=";
+      sha256 = "sha256-2Xr8nZl6Td/fRz3BjxVy+zTWE5VQDxFiGLuYTq2fZXs=";
     };
 
     doCheck = false;
@@ -213,11 +232,11 @@ rec {
 
   freertos_gdb = buildPythonPackage rec {
     pname = "freertos-gdb";
-    version = "1.0.2";
+    version = "1.0.3";
 
     src = fetchPypi {
       inherit pname version;
-      sha256 = "sha256-o0ZoTy7OLVnrhSepya+MwaILgJSojs2hfmI86D9C3cs=";
+      sha256 = "sha256-5rkB01OdbD5Z4vA6dbqhWp5pGwqI1IlE4IE1dSdT1QE=";
     };
 
     doCheck = false;
@@ -232,19 +251,20 @@ rec {
 
   esp-idf-panic-decoder = buildPythonPackage rec {
     pname = "esp-idf-panic-decoder";
-    version = "0.2.0";
+    version = "1.1.0";
 
     format = "pyproject";
     
     src = fetchPypi {
       inherit version;
       pname = "esp_idf_panic_decoder";
-      sha256 = "sha256-t1pg+L7WWVoVZ7weE/CUdMJsgRQvLEUE/GVeJpt0kKI=";
+      sha256 = "sha256-PR8M7VkxAW08QBU8XvE9FwNwejxmXm9GfdrtgMDCmOw=";
     };
 
     doCheck = false;
 
     propagatedBuildInputs = [
+      pyelftools
       setuptools
     ];
 
