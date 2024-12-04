db701f09c63fdbb4489d985612ec7c3cb8b4f145
Quentin Smith <quentin@mit.edu>
2024-12-04 02:42:18 -0500

    Unpack tools like esp-idf does

--- pkgs/esp-idf/tools.nix
+++ pkgs/esp-idf/tools.nix
@@ -50,6 +50,11 @@ let
       sha256 = targetVersionSpec.sha256;
       targetPkgs = toolFhsEnvTargetPackages."${mergedToolSpec.name}" or (_: []);
       exportVars = mergedToolSpec.export_vars;
+      # strip_container_dirs specifies the number of parent directories to remove
+      stripContainerDirs = mergedToolSpec.strip_container_dirs or 0;
+      # export_paths specifies the directories that contain binaries
+      # When there are not binaries to export, a path is specified as [""].
+      exportPaths = builtins.filter (path: path != [""]) (mergedToolSpec.export_paths or []);
     };
 
   mkToolDerivation =
@@ -62,6 +67,8 @@ let
     , sha256
     , targetPkgs
     , exportVars
+    , stripContainerDirs
+    , exportPaths
     }:
 
     let
@@ -71,10 +78,10 @@ let
         runScript = "";
       };
 
-      exportVarsWrapperArgsList = lib.attrsets.mapAttrsToList (name: value: "--set \"${name}\" \"${value}\"") exportVars;
-    in
+      binPaths = map (path: lib.foldl' (a: b: if a == "" then b else "${a}/${b}") "" path) exportPaths;
 
-    stdenv.mkDerivation rec {
+      exportVarsWrapperArgsList = lib.attrsets.mapAttrsToList (name: value: "--set \"${name}\" \"${value}\"") exportVars;
+    in stdenv.mkDerivation (finalAttrs: {
       inherit pname version;
 
       src = fetchurl {
@@ -85,18 +92,24 @@ let
 
       phases = [ "unpackPhase" "installPhase" ];
 
+      setSourceRoot = ''sourceRoot=$(echo ./${lib.strings.replicate stripContainerDirs "*/"})'';
+
+      # Expose binPaths as an array
+      __structuredAttrs = true;
+
+      inherit binPaths;
+
       installPhase = let
         wrapCmd = if (system == "x86_64-linux") || (system == "aarch64-linux") then
-        ''
-          [ ! -d $out/unwrapped_bin ] && mkdir $out/unwrapped_bin
-          WRAPPED_FILE_PATH="$out/unwrapped_bin/$(basename $FILE_PATH)"
-          mv $FILE_PATH $WRAPPED_FILE_PATH
-          makeWrapper ${fhsEnv}/bin/${pname}-env $FILE_PATH --add-flags $WRAPPED_FILE_PATH ${lib.strings.concatStringsSep " " exportVarsWrapperArgsList}
+          ''
+          makeWrapper ${fhsEnv}/bin/${pname}-env "$wrapper_file" --add-flags "$file" ${lib.strings.concatStringsSep " " exportVarsWrapperArgsList}
         ''
       else
       ''
         if [ -n "${lib.strings.concatStringsSep " " exportVarsWrapperArgsList}" ]; then
-          wrapProgram $FILE_PATH ${lib.strings.concatStringsSep " " exportVarsWrapperArgsList}
+          makeWrapper "$file" "$wrapper_file" ${lib.strings.concatStringsSep " " exportVarsWrapperArgsList}
+        else
+          ln -s "$file" "$wrapper_file"
         fi
       '';
       in ''
@@ -105,26 +118,29 @@ let
         # For setting exported variables (see exportVarsWrapperArgsList).
         TOOL_PATH=$out
 
-        for FILE in $(ls $out/bin); do
-          FILE_PATH="$out/bin/$FILE"
-          if [[ -x $FILE_PATH ]]; then
-            ${wrapCmd}
+        for bindir in "''${binPaths[@]}"; do
+          if [ "$bindir" = "bin" ]; then
+            mv $out/bin $out/unwrapped_bin
+            bindir=unwrapped_bin
           fi
+          for file in $out/$bindir/*; do
+            wrapper_file="$out/bin/$(basename "$file")"
+            [ -d "$out/bin" ] || mkdir "$out/bin"
+            ${wrapCmd}
+          done
         done
-
-        # Fix openocd scripts path
-        mkdir $out/openocd-esp32
-        ln -s $out/share $out/openocd-esp32
       '';
 
       meta = with lib; {
         inherit description homepage license;
       };
 
-      passthru = {
-        inherit exportVars;
+      passthru = let
+        inherit (finalAttrs.finalPackage) outPath;
+      in {
+        exportVars = lib.mapAttrs (_: v: lib.replaceStrings ["\${TOOL_PATH}" "$TOOL_PATH"] [outPath outPath] v) exportVars;
       };
-    };
+    });
 
 in
 builtins.listToAttrs (builtins.map (toolSpec: lib.attrsets.nameValuePair toolSpec.name (toolSpecToDerivation toolSpec)) toolSpecList)
