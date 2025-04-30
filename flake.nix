b51d67d82ea6dfe4fb0553fe73a80bb67953c112
Andrew Abbott <andrew@mirx.dev>
2025-04-30 17:59:26 +1000

    Add default devShell

--- flake.nix
+++ flake.nix
@@ -6,42 +6,57 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: {
-    overlays.default = import ./overlay.nix;
-  } // flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
-    in
+  outputs =
     {
-      packages = {
-        inherit (pkgs)
-          esp-idf-full
-          esp-idf-esp32
-          esp-idf-esp32c2
-          esp-idf-esp32c3
-          esp-idf-esp32s2
-          esp-idf-esp32s3
-          esp-idf-esp32c6
-          esp-idf-esp32h2
-          gcc-xtensa-lx106-elf-bin
-          esp8266-rtos-sdk
-          esp8266-nonos-sdk;
-      };
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    {
+      overlays.default = import ./overlay.nix;
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+      in
+      {
+        packages = {
+          inherit (pkgs)
+            esp-idf-full
+            esp-idf-esp32
+            esp-idf-esp32c2
+            esp-idf-esp32c3
+            esp-idf-esp32s2
+            esp-idf-esp32s3
+            esp-idf-esp32c6
+            esp-idf-esp32h2
+            gcc-xtensa-lx106-elf-bin
+            esp8266-rtos-sdk
+            esp8266-nonos-sdk
+            ;
+        };
 
-      devShells = {
-        esp-idf-full = import ./shells/esp-idf-full.nix { inherit pkgs; };
-        esp32-idf = import ./shells/esp32-idf.nix { inherit pkgs; };
-        esp32c2-idf = import ./shells/esp32c2-idf.nix { inherit pkgs; };
-        esp32c3-idf = import ./shells/esp32c3-idf.nix { inherit pkgs; };
-        esp32s2-idf = import ./shells/esp32s2-idf.nix { inherit pkgs; };
-        esp32s3-idf = import ./shells/esp32s3-idf.nix { inherit pkgs; };
-        esp32c6-idf = import ./shells/esp32c6-idf.nix { inherit pkgs; };
-        esp32h2-idf = import ./shells/esp32h2-idf.nix { inherit pkgs; };
-        esp8266-rtos-sdk = import ./shells/esp8266-rtos-sdk.nix { inherit pkgs; };
-        esp8266-nonos-sdk = import ./shells/esp8266-nonos-sdk.nix { inherit pkgs; };
-      };
+        devShells = rec {
+          default = esp-idf-full;
+          esp-idf-full = import ./shells/esp-idf-full.nix { inherit pkgs; };
+          esp32-idf = import ./shells/esp32-idf.nix { inherit pkgs; };
+          esp32c2-idf = import ./shells/esp32c2-idf.nix { inherit pkgs; };
+          esp32c3-idf = import ./shells/esp32c3-idf.nix { inherit pkgs; };
+          esp32s2-idf = import ./shells/esp32s2-idf.nix { inherit pkgs; };
+          esp32s3-idf = import ./shells/esp32s3-idf.nix { inherit pkgs; };
+          esp32c6-idf = import ./shells/esp32c6-idf.nix { inherit pkgs; };
+          esp32h2-idf = import ./shells/esp32h2-idf.nix { inherit pkgs; };
+          esp8266-rtos-sdk = import ./shells/esp8266-rtos-sdk.nix { inherit pkgs; };
+          esp8266-nonos-sdk = import ./shells/esp8266-nonos-sdk.nix { inherit pkgs; };
+        };
 
-      checks = (import ./tests/build-idf-examples.nix { inherit pkgs; }) // (import ./tests/build-esp8266-example.nix { inherit pkgs; });
-    });
+        checks =
+          (import ./tests/build-idf-examples.nix { inherit pkgs; })
+          // (import ./tests/build-esp8266-example.nix { inherit pkgs; });
+      }
+    );
 }
-
