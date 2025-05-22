e67ca91c3406badee06f2c787e01638b2d8ef792
Tim Black <tim@timblack.net>
2025-05-22 15:55:51 -0700

    Fix git dubious ownership for ESP-IDF

--- shells/esp32c5-idf.nix
+++ shells/esp32c5-idf.nix
@@ -3,6 +3,11 @@
 pkgs.mkShell {
   name = "esp-idf-esp32c5-shell";
 
+  shellHook = ''
+    # Fix git dubious ownership for ESP-IDF
+    git config --global --add safe.directory "$IDF_PATH" 2>/dev/null || true
+  '';
+
   buildInputs = with pkgs; [
     ( esp-idf-esp32c5.override { rev = "d930a386dae"; } )
   ];
