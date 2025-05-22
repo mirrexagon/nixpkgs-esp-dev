4659257e288f7fe4892755d32b5ad83d994e4b24
Tim Black <tim@timblack.net>
2025-05-22 16:36:04 -0700

    reimplement git dubious owner fix to use package name directly (which evals to the package's output path - where it is installed in the nix store - instead of the env var which is not reliable and is a race condition.

--- shells/esp32c5-idf.nix
+++ shells/esp32c5-idf.nix
@@ -1,14 +1,17 @@
 { pkgs ? import ../default.nix }:
 
+let
+  esp-idf-custom = pkgs.esp-idf-esp32c5.override { rev = "d930a386dae"; };
+in
 pkgs.mkShell {
   name = "esp-idf-esp32c5-shell";
 
   shellHook = ''
     # Fix git dubious ownership for ESP-IDF
-    git config --global --add safe.directory "$IDF_PATH" 2>/dev/null || true
+    # When you use "${d}" in a Nix string, it automatically converts derivation (d, the build recipe) 
+    # into its output path (where it gets installed in /nix/store/).
+    git config --global --add safe.directory "${esp-idf-custom}" 2>/dev/null || true
   '';
 
-  buildInputs = with pkgs; [
-    ( esp-idf-esp32c5.override { rev = "d930a386dae"; } )
-  ];
+  buildInputs = [ esp-idf-custom ];
 }
